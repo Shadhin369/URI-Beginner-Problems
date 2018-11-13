0c840c59c0c933175851fb2aeba3c5ca2a4fcee0
Shadhin <30951408+Shadhin369@users.noreply.github.com>
2018-11-13 22:44:38 +0600

    Create P1001.java

---
+++ P1001.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class P1001 {
+
+		public static void main(String[] args) {
+			Scanner in = new Scanner(System.in);
+			int A ,B;
+			A = in.nextInt();
+			B = in.nextInt();
+			int X=A+B;
+			System.out.println("X = "+X);
+			in.close();
+		}
+}
