c64427c392633c14a6561fcb83e28cc536b0d171
Shadhin <30951408+Shadhin369@users.noreply.github.com>
2018-11-23 12:48:40 +0600

    Create P1003.java

---
+++ P1003.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class P1003 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+    
+		int A,B;
+		A=input.nextInt();
+		B=input.nextInt();
+		int S=A+B;
+		System.out.println("SOMA = "+S);
+		input.close();
+
+	}
+
+}
