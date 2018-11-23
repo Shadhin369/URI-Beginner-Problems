50f4c18390d174035e7837c9226738b6177e0f9c
Shadhin <30951408+Shadhin369@users.noreply.github.com>
2018-11-23 12:51:41 +0600

    Create P1005.java

---
+++ P1005.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P1005 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+    
+		double a,b,MEDIA;
+		a=input.nextDouble();
+		b=input.nextDouble();
+		MEDIA= ((a*3.5)+(b*7.5))/11;
+		System.out.printf("MEDIA = %.5f\n",MEDIA);
+		input.close();
+
+	}
+
+}
