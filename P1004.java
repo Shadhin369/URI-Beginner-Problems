d5e72b0aed810924b74332cc0243ff74cc66a7a7
Shadhin <30951408+Shadhin369@users.noreply.github.com>
2018-11-23 12:50:30 +0600

    Create P1004.java

---
+++ P1004.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P1004 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+    
+		int a,b,PROD;
+		a= input.nextInt();
+		b= input.nextInt();
+		PROD=a*b;
+		System.out.println("PROD = "+PROD);
+		input.close();
+		
+	}
+
+}
